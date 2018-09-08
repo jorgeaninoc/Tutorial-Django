4af398af75eefe2d678e54940182563c0d11216c
Jorge Niño Cabal <a01172309@itesm.mx>
2018-09-08 15:25:04 -0500

    Tutorial 5 corrected

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -12,7 +12,7 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
     def get_queryset(self):
         """Return the last five published questions (not including those set to be published in the furute)."""
-        return Question.objects.filter(pub_date_lte=timezone.now()).order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
